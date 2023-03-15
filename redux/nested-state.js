c797101f07b4593ae2628323c8f294b39ca18197
Firas <firasdabbabi@gmail.com>
2023-03-15 16:34:07 +0100

    version 2: Import and Use Immer Library

---
+++ redux/nested-state.js
@@ -0,0 +1,54 @@
+// Import Redux and Immer
+const redux = require("redux");
+const createStore = redux.legacy_createStore;
+const produce = require("immer").produce;
+
+// init state
+const initialState = {
+  name: "Robert",
+  address: {
+    stCode: 000,
+    stName: "Main St",
+    city: "Boston",
+    state: "MA",
+  },
+};
+
+// action type
+const STREET_UPDATED = "STREET_UPDATED";
+
+// set action
+const updateStCode = (stCode) => {
+  return {
+    type: STREET_UPDATED,
+    payload: stCode,
+  };
+};
+
+// set Reducer
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case STREET_UPDATED:
+      return produce(state, (draft) => {
+        draft.address.stCode = action.payload;
+      });
+      break;
+
+    default:
+      {
+        return state;
+      }
+      break;
+  }
+};
+
+// set Store and Log it
+const store = createStore(reducer);
+console.log("initial state:", store.getState());
+
+// Subscribe to listen to changes
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated state:", store.getState());
+});
+
+store.dispatch(updateStCode(6533));
